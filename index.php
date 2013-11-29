1bd36b3b489bb4d12b99ef92e82bf3bd6707f2d8
Ynte <s2253216@student.rug.nl>
2013-11-29 16:41:57 +0100

    Added index.php with 4 options

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+        <title>DBWEB - Assignment 1</title>        
+</head>
+<body>
+<?php
+if(isset($_POST['submitq1'])){
+        if(isset($_POST['question1'])){
+                if($_POST['question1'] == 2)
+                        echo 'You answered the question correctly! <br />';
+                else
+                        echo 'Wrong answer! Please try again! <br />';
+        } else
+                echo 'Nothing selected! <br />';        
+} else {
+?>
+        <form action="index.php" method="post">
+                Which name starts with an 'a'? <br />
+                <input type="radio" name="question1" value="1" />Bob<br />
+                <input type="radio" name="question1" value="2" />Alice<br />
+                <input type="radio" name="question1" value="3" />John<br />
+                <input type="radio" name="question1" value="4" />Dean<br />
+                <input type="submit" name="submitq1" value="Submit" />
+        </form>
+<?php
+}
+?>
+</body>
+</html>
