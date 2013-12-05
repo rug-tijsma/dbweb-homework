47e55f87734a366d6e10821e5c2aa76ca26745d3
Ynte <s2253216@student.rug.nl>
2013-12-05 12:33:30 +0100

    Added create_DB.sql and test_data.sql

---
+++ test_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO `choice` (`c_number`, `c_text`, `correct`) VALUES
+(1, 'Orange', 1),
+(1, 'Yellow', 0),
+(1, 'Green', 0),
+(2, 'Orange', 0),
+(2, 'Yellow', 1),
+(2, 'Green', 0),
+(3, 'Orange', 0),
+(3, 'Yellow', 0),
+(3, 'Pear', 1),
+
+INSERT INTO `question` (`q_number`, `q_text`) VALUES
+(1, 'The color of an orange is:'),
+(2, 'The color of a banana is:'),
+(3, 'The color of a pear is:'),
