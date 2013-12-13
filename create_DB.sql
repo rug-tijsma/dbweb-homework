1a9115e538efd649ae2a936385a740731678cfbc
Ynte <s2253216@student.rug.nl>
2013-12-13 15:04:03 +0100

    Processed given feedback

--- create_DB.sql
+++ create_DB.sql
@@ -1,12 +1,6 @@
 CREATE DATABASE IF NOT EXISTS `dbweb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `dbweb`;
 
-CREATE TABLE IF NOT EXISTS question (
-  q_number int(255) NOT NULL,
-  q_text varchar(255) NOT NULL,
-  PRIMARY KEY (q_number),
-);
-
 CREATE TABLE IF NOT EXISTS choice (
   q_number int(255) NOT NULL,
   c_number int(255) NOT NULL,
@@ -14,5 +8,11 @@ CREATE TABLE IF NOT EXISTS choice (
   correct tinyint(1) NOT NULL,
   PRIMARY KEY (q_number, c_number),
   FOREIGN KEY (q_number) references question(q_number)
+);
+
+CREATE TABLE IF NOT EXISTS question (
+  q_number int(255) NOT NULL,
+  q_text varchar(255) NOT NULL,
+  PRIMARY KEY (q_number),
 );
  
--- test_data.sql
+++ test_data.sql
@@ -1,3 +1,7 @@
+INSERT INTO question (q_number, q_text) VALUES(1, 'What is the color of an orange?');
+INSERT INTO question (q_number, q_text) VALUES(2, 'What is the color of a banana?');
+INSERT INTO question (q_number, q_text) VALUES(3, 'What is the color of a pear?');
+
 INSERT INTO choice (q_number, c_number, c_text, correct) VALUES(1, 1, 'Orange', 1);
 INSERT INTO choice (q_number, c_number, c_text, correct) VALUES(1, 2, 'Yellow', 0);
 INSERT INTO choice (q_number, c_number, c_text, correct) VALUES(1, 3, 'Green', 0);
@@ -6,8 +10,4 @@ INSERT INTO choice (q_number, c_number, c_text, correct) VALUES(2, 5, 'Yellow', 
 INSERT INTO choice (q_number, c_number, c_text, correct) VALUES(2, 6, 'Green', 0);
 INSERT INTO choice (q_number, c_number, c_text, correct) VALUES(3, 7, 'Orange', 0);
 INSERT INTO choice (q_number, c_number, c_text, correct) VALUES(3, 8, 'Yellow', 0);
-INSERT INTO choice (q_number, c_number, c_text, correct) VALUES(3, 9, 'Pear', 1);
-
-INSERT INTO question (q_number, q_text) VALUES(1, 'What is the color of an orange?');
-INSERT INTO question (q_number, q_text) VALUES(2, 'What is the color of a banana?');
-INSERT INTO question (q_number, q_text) VALUES(3, 'What is the color of a pear?');
+INSERT INTO choice (q_number, c_number, c_text, correct) VALUES(3, 9, 'Green', 1);
