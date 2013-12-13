4d4bf1fa605edb2836ef8e1b683bc6071bf76271
Ynte <s2253216@student.rug.nl>
2013-12-13 15:23:39 +0100

    Added session management

--- index.php
+++ index.php
@@ -1,109 +1,117 @@
-<?php include 'databaselogin.php';?>
+<?php
+session_start();
+
+include_once("db-config.php");
+try {
+    $db = new PDO("mysql:host=localhost;dbname=dbweb",$username,$password); 
+}	
+    catch (PDOException $e){
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 604800)) {
+    session_unset();     
+    session_destroy(); 
+}
+?>
 
 <html>
 <head>
-    <title>DBWEB - Assignment 2</title>
+    <title>DBWEB - Assignment 3</title>
 </head>
 <body>
 <?php
-
-if(!isset($_POST['submit']) && !isset($_POST['nextQuestion'])) {
-    $questionNumber = 1;
-} else {
-	if(!isset($_COOKIE['Koekje_q'])) {
-		$questionNumber = 1;
-	} else {
-		$questionNumber = $_COOKIE['Koekje_q'];
-	}
+	
+if(!isset($_POST['submit']) && !isset($_SESSION['currentQ'])) {
+    $_SESSION['currentQ'] = 1;
+}
+if(!isset($_SESSION['correctA'])) {
+	$_SESSION['correctA'] = 0;
 }
 	
 $num_q = 'SELECT q_number FROM question';
-$res_num_q = mysql_query($num_q);
+$res_num_q = $db->query($num_q);
 
 if(!$res_num_q) {
 	echo 'Couldn\'t run query ' . mysql_error();
 	exit();
 }
 
-if( mysql_num_rows($res_num_q) == 0) {
+if($res_num_q->rowCount() == 0) {
 	echo 'Couldn\'t find any questions ' . mysql_error();
 	exit();
 }
 
 if(isset($_POST['submit'])){
+
     if(isset($_POST['answer'])){
+		$_SESSION['LAST_ACTIVITY'] = time();			
 		$c = 'SELECT correct FROM choice 
-			WHERE c_number = '. $_POST['answer'] . ' AND q_number = '. $questionNumber;
-		$res_c = mysql_query($c);
+			WHERE c_number = '. $_POST['answer'] . ' AND q_number = '. $_SESSION['currentQ'];
+		$res_c = $db->query($c);
 		
 		if (!$res_c) {
 			echo 'Couldn\'t run query ' . mysql_error();
 			exit();
 		}
 		
-		if (mysql_num_rows($res_c) == 0) {
+		if ($res_c->rowCount() == 0) {
 			echo 'Couldn\'t find the given choice or the questions';
 			exit();
 		}
 		
-		$row_c = mysql_fetch_assoc($res_c);
+		$row_w = $res_c->fetch(PDO::FETCH_ASSOC);
 		
-        if($row_c['correct'])
-            echo 'You answered the question correctly! <br />';
+        if($row_w['correct'])
+            echo 'You answered the question correctly! <br /> <br />';
         else
-            echo 'Wrong answer! <br />';
+            echo 'Wrong answer! <br /> </br>';
 			
-		if(!isset($_COOKIE['Koekje_a'])) {
-			setCookie('Koekje_a', $row_c['correct'], time()+3600);
+		if(!isset($_SESSION['correctA'])) {
+			$_SESSION['correctA'] = $row_w['correct'];
 		} else {
-			setCookie('Koekje_a', ($row_c['correct'] + $_COOKIE['Koekje_a']), time()+3600);
+			$_SESSION['correctA'] = ($row_w['correct']+$_SESSION['correctA']);
 		}
         
-		$questionNumber++;
-          
-        if($questionNumber < mysql_num_rows($res_num_q) + 1){
-		?>
-			<form action="index.php" method="post">
-			<input type="submit" name="nextQuestion" value="Next" />
-			<input type="hidden" name="questionNumber" value="<?php echo $questionNumber; ?>" />
-			</form>
-		<?php
-			setCookie('Koekje_q', $questionNumber, time()+3600);
-        } else {
-			//Last question. Cookie isn't sent yet so need to update the count of correct answers.	
-			echo 'No more questions to answer. You answered ' . ($_COOKIE['Koekje_a'] + $row_c['correct']) . 
-				' questions out of ' . mysql_num_rows($res_num_q) . ' questions correctly.';
+		   
+        if($_SESSION['currentQ'] == $res_num_q->rowCount()){
+			echo 'No more questions to answer. You answered ' . $_SESSION['correctA'] . 
+				' questions out of ' . $res_num_q->rowCount() . ' questions correctly.';
+				session_unset();     
+				session_destroy(); 
+				exit;
 		}
+		else
+			$_SESSION['currentQ'] = $_SESSION['currentQ'] + 1;        
     } else
         echo 'Nothing selected! <br />';
 }
   
-if (($questionNumber == 1 || isset($_POST['nextQuestion'])) && $questionNumber <= mysql_num_rows($res_num_q)){
+if ($_SESSION['currentQ'] >= 0 && $_SESSION['currentQ'] <= $res_num_q->rowCount()){
 
 	$q = 'SELECT q_text FROM question 
-		WHERE q_number = ' . $questionNumber;
+		WHERE q_number = ' . $_SESSION['currentQ'];
 	$a = 'SELECT c_text, c_number FROM choice
-		WHERE q_number = ' . $questionNumber;
-	$res_q = mysql_query($q);
-	$res_a = mysql_query($a);
+		WHERE q_number = ' . $_SESSION['currentQ'];;
+	$res_q = $db->query($q);
+	$res_a = $db->query($a);
 	
 	if (!$res_q || !$res_a) {
 		echo 'Couldn\'t run query ' . mysql_error();
 		exit();
 	}
 	
-	if (mysql_num_rows($res_q) == 0 || mysql_num_rows($res_a) == 0) {
+	if ($res_q->rowCount() == 0 || $res_a->rowCount() == 0) {
 		echo 'No questions found ' . mysql_error();
 		exit();
 	}
 	
-	$row_q = mysql_fetch_assoc($res_q);
-  
+	$row_q = $res_q->fetch(PDO::FETCH_ASSOC);
 	?>
-	<form action="index.php" method="post">
-	<input type="hidden" name="questionNumber" value="<?php echo $questionNumber; ?>" />
+	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+	<input type="hidden" name="questionNumber" value="<?php echo $_SESSION['currentQ']; ?>" />
 	<?php echo $row_q['q_text'] .'<br />';
-    while ($row_a = mysql_fetch_assoc($res_a)) {
+    while ($row_a = $res_a->fetch(PDO::FETCH_ASSOC)) {
 		echo '<input type="radio" name="answer" value="'. $row_a['c_number'] .'" />'. $row_a['c_text']. ' <br />';
 	}
     ?>
